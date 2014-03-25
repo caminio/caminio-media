74e54e430a192cd7d33e7f5b15d934e9c80b1bb8
thorsten zerha <thorsten.zerha@tastenwerk.com>
2014-03-25 16:30:51 +0100

    thumbnail work

--- assets/javascripts/caminio-media/models/mediafile.js
+++ assets/javascripts/caminio-media/models/mediafile.js
@@ -18,13 +18,27 @@
       if( currentDomain.preferences.isCaminioHosted )
         return '/caminio/domains/'+currentDomain._id+'/preview/'+this.get('name');
       return null;
-    }.property('preferences'),
+    }.property(),
     isImage: function(){
       return this.get('contentType').indexOf('image') === 0;
     }.property('contentType'),
     humanSize: function(){
       return filesize(this.get('size'));
-    }.property('size')
+    }.property('size'),
+    preferencesAsStr: function(){
+      return JSON.stringify(this.get('preferences'));
+    }.property('preferences'),
+    getThumbDim: function( dim ){
+      if( this.get('preferences').thumbs && this.get('preferences').thumbs[dim] )
+        return this.get('preferences').thumbs[dim];
+      return null;
+    },
+    setThumbDim: function( dim, val ){
+      var pref = this.get('preferences');
+      pref.thumbs = pref.thumbs || {};
+      pref.thumbs[dim] = val;
+      this.set('preferences', pref);
+    }
   });
 
 })(App);
